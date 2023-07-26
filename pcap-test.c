d4fdb67c3142a3dfeffffb059bc756455c7781f4
idkdyk <syousz253@gmail.com>
2023-07-26 20:13:47 +0900

    Add pcap-test.c

---
+++ pcap-test.c
@@ -0,0 +1,208 @@
+#include <pcap.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+void usage() {
+    printf("syntax: pcap-test <interface>\n");
+    printf("sample: pcap-test wlan0\n");
+}
+
+#define ETHER_ADDR_LEN 6
+struct libnet_ethernet_hdr
+{
+    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
+    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
+    u_int16_t ether_type;                 /* protocol */
+};
+
+struct libnet_ipv4_hdr
+{
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t ip_hl:4,      /* header length */
+        ip_v:4;         /* version */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t ip_v:4,       /* version */
+        ip_hl:4;        /* header length */
+#endif
+    u_int8_t ip_tos;       /* type of service */
+#ifndef IPTOS_LOWDELAY
+#define IPTOS_LOWDELAY      0x10
+#endif
+#ifndef IPTOS_THROUGHPUT
+#define IPTOS_THROUGHPUT    0x08
+#endif
+#ifndef IPTOS_RELIABILITY
+#define IPTOS_RELIABILITY   0x04
+#endif
+#ifndef IPTOS_LOWCOST
+#define IPTOS_LOWCOST       0x02
+#endif
+    u_int16_t ip_len;         /* total length */
+    u_int16_t ip_id;          /* identification */
+    u_int16_t ip_off;
+#ifndef IP_RF
+#define IP_RF 0x8000        /* reserved fragment flag */
+#endif
+#ifndef IP_DF
+#define IP_DF 0x4000        /* dont fragment flag */
+#endif
+#ifndef IP_MF
+#define IP_MF 0x2000        /* more fragments flag */
+#endif
+#ifndef IP_OFFMASK
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+#endif
+    u_int8_t ip_ttl;          /* time to live */
+    u_int8_t ip_p;            /* protocol */
+    u_int16_t ip_sum;         /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest address */
+};
+
+struct libnet_tcp_hdr
+{
+    u_int16_t th_sport;       /* source port */
+    u_int16_t th_dport;       /* destination port */
+    u_int32_t th_seq;          /* sequence number */
+    u_int32_t th_ack;          /* acknowledgement number */
+#if (LIBNET_LIL_ENDIAN)
+    u_int8_t th_x2:4,         /* (unused) */
+        th_off:4;        /* data offset */
+#endif
+#if (LIBNET_BIG_ENDIAN)
+    u_int8_t th_off:4,        /* data offset */
+        th_x2:4;         /* (unused) */
+#endif
+    u_int8_t  th_flags;       /* control flags */
+#ifndef TH_FIN
+#define TH_FIN    0x01      /* finished send data */
+#endif
+#ifndef TH_SYN
+#define TH_SYN    0x02      /* synchronize sequence numbers */
+#endif
+#ifndef TH_RST
+#define TH_RST    0x04      /* reset the connection */
+#endif
+#ifndef TH_PUSH
+#define TH_PUSH   0x08      /* push data to the app layer */
+#endif
+#ifndef TH_ACK
+#define TH_ACK    0x10      /* acknowledge */
+#endif
+#ifndef TH_URG
+#define TH_URG    0x20      /* urgent! */
+#endif
+#ifndef TH_ECE
+#define TH_ECE    0x40
+#endif
+#ifndef TH_CWR
+#define TH_CWR    0x80
+#endif
+    u_int16_t th_win;         /* window */
+    u_int16_t th_sum;         /* checksum */
+    u_int16_t th_urp;         /* urgent pointer */
+};
+
+
+
+void printf_mac(uint8_t *m){
+    printf("%02x:%02x:%02x:%02x:%02x:%02x:",m[0],m[1],m[2],m[3],m[4],m[5]);
+}
+
+void print_ip(uint8_t *ip){
+    printf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+}
+
+
+void print_port(uint16_t port){
+    printf("%hu", port);
+}
+
+void print_payload(const u_char *payload, int len) {
+    int max_bytes = (len < 10) ? len : 10; // 최대 10바이트까지만 출력
+    for (int i = 0; i < max_bytes; i++) {
+        printf("%02x ", payload[i]);
+    }
+    printf("\n");
+}
+
+typedef struct {
+    char* dev_;
+} Param;
+
+Param param = {
+    .dev_ = NULL
+};
+
+bool parse(Param* param, int argc, char* argv[]) {
+    if (argc != 2) {
+        usage();
+        return false;
+    }
+    param->dev_ = argv[1];
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (!parse(&param, argc, argv))
+        return -1;
+
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
+    if (pcap == NULL) {
+        fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+        return -1;
+    }
+
+    while (true) {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(pcap, &header, &packet);
+        if (res == 0) continue;
+        if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+            break;
+        }
+        printf("%u bytes captured\n", header->caplen);
+        printf("\n");
+        struct libnet_ethernet_hdr *eth_hdr = (struct libnet_ethernet_hdr *)packet;
+        printf("smac ");
+        printf_mac(eth_hdr->ether_shost);
+        printf("\n");
+        printf("dmac ");
+        printf_mac(eth_hdr->ether_dhost);
+        printf("\n");
+        if(ntohs(eth_hdr->ether_type) !=0x0800)
+            continue;
+        printf_mac(eth_hdr->ether_dhost);
+        printf("\n");
+        struct libnet_ipv4_hdr *ip_hdr = (struct libnet_ipv4_hdr *)(packet + sizeof(struct libnet_ethernet_hdr));
+        printf("src_ip ");
+        print_ip((uint8_t *)&(ip_hdr->ip_src));
+        printf("\n");
+
+        printf("dst_ip ");
+        print_ip((uint8_t *)&(ip_hdr->ip_dst));
+        printf("\n");
+        struct libnet_tcp_hdr *tcp_prt = (struct libnet_tcp_hdr *)(packet + sizeof(struct libnet_ethernet_hdr)+ sizeof(struct libnet_ipv4_hdr));
+        printf("src_port ");
+        print_port(ntohs(tcp_prt->th_sport));
+        printf("\n");
+        printf("dst_port ");
+        print_port(ntohs(tcp_prt->th_dport));
+        printf("\n");
+
+        int payload_offset = sizeof(struct libnet_ethernet_hdr) + sizeof(struct libnet_ipv4_hdr) + sizeof(struct libnet_tcp_hdr);
+        int payload_length = header->caplen - payload_offset;
+        if (payload_length > 0) {
+            printf("Payload(Data) (hexadecimal, up to 10 bytes): ");
+            print_payload(packet + payload_offset, payload_length);
+            printf("\n");
+        }
+        printf("\n");
+        printf("------------------");
+    }
+
+    pcap_close(pcap);
+}
